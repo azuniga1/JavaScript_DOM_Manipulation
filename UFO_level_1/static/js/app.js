c8b697774334b0c956938f3cf4cd5aedee6d82c7
Ariel Zuniga <azuniga2003@gmail.com>
2019-12-08 14:58:49 -0700

    update app.js

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -2,7 +2,6 @@
 // YOUR CODE HERE!
 var tbody = d3.select("tbody")
 
-console.log(data);
 
 data.forEach(function(ufosighting) {
     console.log(ufosighting);
@@ -13,4 +12,36 @@ data.forEach(function(ufosighting) {
         var cell = row.append("td");
         cell.text(value);
     });
+});
+
+var ufosighting = data;
+
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    var filteredData = ufosighting.filter(dates => dates.datetime === inputValue);
+
+    //console.log(filteredData);
+
+    d3.selectAll("tr").remove();
+
+    filteredData.forEach(function(filterdates) {
+        console.log(filterdates)
+        var row = tbody.append("tr");
+
+        Object.entries(filterdates).forEach(function([key, value]) {
+            console.log(key, value);
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+    
 });
